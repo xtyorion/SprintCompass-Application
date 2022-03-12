d0b0ff97faab4519ddd00376e740f962594fae19
mejoradavincentflorence <mejoradavincentflorence@gmail.com>
2022-03-12 13:20:21 -0500

    updated home tab navigation

--- screens/UserHomeNavigatorScreen.js
+++ screens/UserHomeNavigatorScreen.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Button, Headline, Paragraph } from 'react-native-paper';
-import Background from '../components/Background';
-import Logo from '../components/Logo';
-import {styles} from '../styles/styles';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native-paper';
 import UserHomeScreen from './UserHomeScreen';
 import LikesListScreen from './LikesListScreen';
 import MessageListScreen from './MessageListScreen';
 import ProfileScreen from './ProfileScreen';
+//icons
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 
 const UserHomeNavigatorScreen = props => {
@@ -16,14 +16,41 @@ const UserHomeNavigatorScreen = props => {
 
     return (
       <Tab.Navigator  
-      screenOptions={{
-        headerShown: false
-      }}>
-          <Tab.Screen name="Home" component={UserHomeScreen}/>
-          <Tab.Screen name="Likes" component={LikesListScreen}/>
-          <Tab.Screen name="Messages" component={MessageListScreen}/>
-          <Tab.Screen name="Profile" component={ProfileScreen}/>
-        </Tab.Navigator>
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          showLabel: false,
+          tabBarStyle: {
+            position: 'absolute',
+            bottom: 25,
+            left: 20,
+            right: 20,
+            elevation: 0,
+            backgroundColor: 'transparent',
+            height: 90,
+          }
+        }}>
+        <Tab.Screen name="Home" component={UserHomeScreen} options={{
+          tabBarIcon: ({focused, size }) => (
+            <Ionicons name={focused ? "home" : "home-outline"} color={'#826cff'} size={size} />
+          ),
+        }}/>
+        <Tab.Screen name="Likes" component={LikesListScreen} options={{
+          tabBarIcon: ({focused, size}) => (
+            <Ionicons name={focused ? "heart" : "heart-outline"} color={'#826cff'} size={size} />
+          )
+        }}/>
+        <Tab.Screen name="Messages" component={MessageListScreen} options={{
+          tabBarIcon: ({focused, size}) => (
+            <Ionicons name={focused ? "mail-open" : "mail-outline"} color={'#826cff'} size={size} />
+          )
+        }}/>
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarIcon: ({focused, size}) => (
+            <FontAwesome name={focused ? "user" : "user-o"} color={'#826cff'} size={size} />
+          )
+        }}/>
+      </Tab.Navigator>
     );
 }
 
